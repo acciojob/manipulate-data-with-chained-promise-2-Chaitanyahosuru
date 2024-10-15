c2edb65efc261f772a7fbda59309f6b3fb0588ca
Chaitanyahosuru <113969435+Chaitanyahosuru@users.noreply.github.com>
2024-10-15 20:37:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -12,19 +12,16 @@ getData()
   .then(data => {
     // Filter out odd numbers
     const evenNumbers = data.filter(num => num % 2 === 0);
-    // Update output after 1 second
-    setTimeout(() => {
-      document.getElementById('output').innerText = evenNumbers.join(', ');
-    }, 1000);
+    document.getElementById('output').innerText = evenNumbers.join(', ');
     return evenNumbers;
   })
   .then(evenNumbers => {
     // Multiply even numbers by 2
     const doubledNumbers = evenNumbers.map(num => num * 2);
-    // Update output after another 2 seconds
     setTimeout(() => {
       document.getElementById('output').innerText = doubledNumbers.join(', ');
     }, 2000);
   });
+
 
 
